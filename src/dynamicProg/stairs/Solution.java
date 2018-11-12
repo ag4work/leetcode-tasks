561af11f60622783f7cd6b52c37ad2cc29969b5b
flamenco <ag4work@gmail.com>
2018-11-13 01:17:59 +0300

    stairs climber

--- src/dynamicProg/stairs/Solution.java
+++ src/dynamicProg/stairs/Solution.java
@@ -5,8 +5,7 @@ class Solution {
     int cnt = 0;
 
     public int climbStairs(int n) {
-        f(n);
-        return cnt;
+        return fibb(n+1);
     }
 
     void f(int n) {
@@ -14,16 +13,34 @@ class Solution {
             cnt++;
             return;
         }
-        if (n>0) {
-            f(n-1);
+        if (n > 0) {
+            f(n - 1);
         }
-        if (n>1) {
-            f(n-2);
+        if (n > 1) {
+            f(n - 2);
         }
     }
 
+    static int fibb(int n) {
+        int a = 1;
+        int b = 1;
+        if (n <= 2) {
+            return 1;
+        }
+        int sum = 0;
+        for (int i = 3; i <= n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
-        System.out.println((new Solution().climbStairs(3)));
+        for (int i = 1; i <= 44; i++) {
+            System.out.println(i + " " + new Solution().climbStairs(i) + " " + fibb(i+1) );
+//            System.out.println(i + " " + fibb(i+1) );
+        }
+//        System.out.println(44 + " " + new Solution().climbStairs(44));
     }
 }
