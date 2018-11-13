5fb6fc8f2543e452b33d92f5f8491d15fa547545
flamenco <ag4work@gmail.com>
2018-11-14 00:56:46 +0300

    reverse string

---
+++ src/ReverseString/RevStrNew.java
@@ -0,0 +1,16 @@
+package ReverseString;
+
+
+public class RevStrNew {
+    public String reverseString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(s.length()-i-1));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RevStrNew().reverseString(""));
+    }
+}
