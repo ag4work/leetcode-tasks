f97b0f985a1484516c660b4ed1ee798bc1a203e0
flamenco <ag4work@gmail.com>
2018-11-16 00:26:09 +0300

    merge two LinkedLists

--- src/linkedList/mergeSortedLinkedList/Solution.java
+++ src/linkedList/mergeSortedLinkedList/Solution.java
@@ -9,6 +9,8 @@ Merge two sorted linked lists and return it as a new list. The new list should b
         Output: 1->1->2->3->4->4
 */
 
+import java.util.List;
+
 public class Solution {
 
 
@@ -30,89 +32,47 @@ public class Solution {
         return listNode;
     }
 
-    public ListNode mergeTwoLists(ListNode a1, ListNode a2) {
-        if (a1 == null && a2 == null) {
-            return null;
-        }
-        ListNode curA = a1;
-        ListNode curB = a2;
-        ListNode cur = null;
-        ListNode head = null;
-        while (curA != null && curB != null) {
-            boolean moveA = false;
-            if (curA.val <= curB.val) {
-//                System.out.println("сдвинулись по А");
-                if (cur == null) {
-                    cur = new ListNode(curA.val);
-                    head = cur;
-//                    System.out.println("Create first A element");
-                } else {
-                    cur.next = new ListNode(curA.val);
-//                    System.out.println("Create new A element");
-                    cur = cur.next;
-                }
-                moveA = true;
-            }
-            boolean moveB = false;
-            if (curA.val >= curB.val) {
-//                System.out.println("сдвинулись по B");
-                if (cur == null) {
-                    cur = new ListNode(curB.val);
-                    head = cur;
-//                    System.out.println("Create first B element");
-                } else {
-//                    System.out.println("Create new B element");
-                    cur.next = new ListNode(curB.val);
-                    cur = cur.next;
-                }
-                moveB = true;
-            }
-            if (moveA) {
-                curA = curA.next;
-            }
-            if (moveB) {
-                curB = curB.next;
-            }
-//            head.print();
-        }
-//        System.out.println("Конец основного цикла");
-        ListNode tail = null;
-        if (curA != null) {
-//            System.out.println("Добавляем остаток из А");
-            tail = curA;
-        } else if (curB != null) {
-//            System.out.println("Добавляем остаток из B");
-            tail = curB;
-        }
-//        System.out.println("Остаток:");
-//        tail.print();
-        while (tail != null) {
-            if (cur == null) {
-                cur = new ListNode(tail.val);
-                head = cur;
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null) return b;
+        if (b == null) return a;
+        ListNode cur = new ListNode(0); // first is dummy
+        ListNode head = cur;
+        ListNode prev = null;
+        while (a != null && b != null) {
+            System.out.println("a.val: " + a.val + "   b.val: " + b.val);
+            if (a.val < b.val) {
+                cur.next = new ListNode(a.val);
+                a = a.next;     System.out.println("сдвигаемся по А");
+                prev = b;
             } else {
-                cur.next = new ListNode(tail.val);
-                cur = cur.next;
+                cur.next = new ListNode(b.val);
+                b = b.next;     System.out.println("сдвигаемся по B");
+                prev = a;
             }
-
-            tail = tail.next;
+            cur = cur.next;
+            head.print();
         }
-        return head;
+        while (prev != null) {
+            cur.next = new ListNode(prev.val);
+            cur = cur.next;
+            prev = prev.next;
+        }
+        return head.next;
     }
 
 
     public static void main(String[] args) {
-        ListNode a3 = new ListNode(3, null);
+        ListNode a3 = new ListNode(4, null);
         ListNode a2 = new ListNode(2, a3);
         ListNode a1 = new ListNode(1, a2);
 
-//        ListNode b3 = new ListNode(8, null);
-//        ListNode b2 = new ListNode(7, b3);
-        ListNode b1 = null;
-//        a1.print();
-//        b1.print();
+        ListNode b3 = new ListNode(4, null);
+        ListNode b2 = new ListNode(3, b3);
+        ListNode b1 = new ListNode(1, b2);
+        a1.print();
+        b1.print();
 
-        ListNode mergedList = new Solution().mergeTwoLists(null, a3);
+        ListNode mergedList = new Solution().mergeTwoLists(a1, b1);
         mergedList.print();
 
     }
