6cc964087a4188e020cc4b0c8d11db23a6d7b465
flamenco <ag4work@gmail.com>
2018-11-20 00:44:23 +0300

    BinaryTreeLevelOrderTraversal (slow - need to use BFS)

--- src/trees/BinaryTreeLevelOrderTraversal/Solution.java
+++ src/trees/BinaryTreeLevelOrderTraversal/Solution.java
@@ -7,42 +7,62 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return new ArrayList<>();
 
-        List<TreeNode> nodes = new ArrayList<TreeNode>() {{ add(root); }};
+        List<TreeNode> nodes = new ArrayList<TreeNode>() {{
+            add(root);
+        }};
+        List<Integer> rootInt = new ArrayList<Integer>() {{
+            add(root.val);
+        }};
+        List<List<Integer>> res = new ArrayList<List<Integer>>() {{
+            add(rootInt);
+        }};
+
         do {
             List<TreeNode> childs = new ArrayList<>();
             for (TreeNode node : nodes) {
-                childs.addAll(getDirectChilds(node));
+                //childs.addAll(getDirectChilds(node));
+                safeAdd(childs, node.left);
+                safeAdd(childs, node.right);
             }
-            res.add(childs.stream().map(r->r.val).collect(Collectors.toList()));
+            List<Integer> childInts = getChildInt(childs);
+            if (!childInts.isEmpty()) {
+                res.add(childInts);
+            }
             nodes = childs;
         } while (nodes.size() > 0);
         return res;
     }
 
-    private List<TreeNode> getDirectChilds(TreeNode node) {
-        List<TreeNode> res = new ArrayList<>();
-        if (node.left != null) {
-            res.add(node.left);
+    private void safeAdd(List<TreeNode> childs, TreeNode node) {
+        if (node != null) {
+            childs.add(node);
         }
-        if (node.right != null) {
-            res.add(node.right);
-        }
+    }
 
-        return res;
+    private List<Integer> getChildInt(List<TreeNode> nodes) {
+        List<Integer> list = new ArrayList<>();
+        for (TreeNode node : nodes) {
+            if (node == null) continue;
+            list.add(node.val);
+        }
+        return list;
     }
 
     public static void main(String[] args) {
-
+        TreeNode tree = creatBST();
+        List<List<Integer>> lists = new Solution().levelOrder(tree);
+        System.out.println(lists);
     }
-        private static TreeNode creatBST() {
+
+    private static TreeNode creatBST() {
         TreeNode treeNode = new TreeNode(4);
         TreeNode left1 = new TreeNode(1);
         TreeNode right1 = new TreeNode(6);
 
-        treeNode.left = new TreeNode(1);
-        treeNode.right = new TreeNode(6);
+        treeNode.left = left1;
+        treeNode.right = right1;
 
         TreeNode left2 = new TreeNode(5);
         TreeNode right2 = new TreeNode(7);
