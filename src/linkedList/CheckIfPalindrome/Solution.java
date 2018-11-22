9032044960a75383e0ae2f0d2aae9a4abb6e3f82
flamenco <ag4work@gmail.com>
2018-11-23 02:24:24 +0300

    linked list palindrom check

--- src/linkedList/CheckIfPalindrome/Solution.java
+++ src/linkedList/CheckIfPalindrome/Solution.java
@@ -7,27 +7,72 @@ import java.util.Objects;
 public class Solution {
 
     public boolean isPalindrome(ListNode head) {
-        return false;
+        if (head.next == null || head == null) return true;
+        int len = len(head);
+        ListNode secondHalfRoot = getSecondHalfRoot(head, len);
+        ListNode rev = reverse(secondHalfRoot);
+        //rev.print();
+
+        ListNode firstHalfEnd = getFirstHalfEnd(head, len);
+        firstHalfEnd.next = null;
+       // head.print();
+        return eq(rev, head);
     }
 
-    public static void main(String[] args) {
 
 
-        ListNode a5 = new ListNode(1, null);
-        ListNode a4 = new ListNode(2, a5);
-        ListNode a3 = new ListNode(3, a4);
-        ListNode a2 = new ListNode(2, a3);
-        ListNode a1 = new ListNode(1, a2);
-
-        a1.print();
-        System.out.println(len(a1));
-        System.out.println(getSecondHalfRoot(a1, len(a1)));
-
+    private static boolean eq(ListNode a, ListNode b) {
+        ListNode curA = a;
+        ListNode curB = b;
+        while(!(curA == null && curB == null)) {
+            if (!simpleEq(curA, curB)) {
+                return false;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return true;
     }
 
+    private static boolean simpleEq(ListNode a, ListNode b) {
+        if (a==null && b == null) {
+            return true;
+        }
+        if (a == null ^ b == null) {
+            return false;
+        }
+        return a.val == b.val;
+    }
+    private static ListNode reverse(ListNode head) {
+        if (head.next == null) return head;
+        ListNode prev = head;
+        ListNode cur = head.next;
+
+        ListNode tmpCurNext;
+
+        do {
+            tmpCurNext = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmpCurNext;
+        } while (tmpCurNext != null);
+        head.next.next = head;
+        head.next = null;
+        return prev;
+    }
     private static ListNode getSecondHalfRoot(ListNode root, int length) {
+        if (length == 1) return root;
         ListNode cur = root;
-        for (int i = 0; i < length / 2; i++) {
+        for (int i = 0; i < length / 2 + length % 2; i++) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+
+    private static ListNode getFirstHalfEnd(ListNode root, int length) {
+        if (length == 1) return root;
+        ListNode cur = root;
+        for (int i = 0; i < length / 2 - 1; i++) {
             cur = cur.next;
         }
         return cur;
@@ -40,11 +85,31 @@ public class Solution {
         while (cur.next != null) {
             cur = cur.next;
             c++;
-
         }
         return c;
     }
+
+    public static void main(String[] args) {
+
+
+//        ListNode a5 = new ListNode(2, null);
+//        ListNode a4 = new ListNode(1, a5);
+//        ListNode a5 = new ListNode(1, null);
+//        ListNode a4 = new ListNode(2, a5);
+//        ListNode a3 = new ListNode(3, a4);
+//        ListNode a2 = new ListNode(2, a3);
+//        ListNode a1 = new ListNode(1, a2);
+//        a1.print();
+        ListNode a4 = new ListNode(1, null);
+        ListNode a3 = new ListNode(2, a4);
+        ListNode a2 = new ListNode(2, a3);
+        ListNode a1 = new ListNode(1, a2);
+        System.out.println(new Solution().isPalindrome(a1));
+    }
 }
+
+
+
 
 class ListNode {
     int val;
