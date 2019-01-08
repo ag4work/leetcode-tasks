692185c3045221866f2a300fc2bd199d4754f188
flamenco <ag4work@gmail.com>
2019-01-08 03:57:15 +0300

    group anagram - performance improved

--- src/medium/arrays/GroupAnagram/Solution.java
+++ src/medium/arrays/GroupAnagram/Solution.java
@@ -4,33 +4,24 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<List<Character>, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
-            List<Character> chars = toCharSetSorted(str);
-            if (!map.containsKey(chars)) {
+            String sortedStr = toCharSetSorted(str);
+            if (!map.containsKey(sortedStr)) {
                 List<String> l = new LinkedList<>();
                 l.add(str);
-                map.put(chars, l);
+                map.put(sortedStr, l);
             } else {
-                map.get(chars).add(str);
+                map.get(sortedStr).add(str);
             }
         }
-        List<List<String>> res = new LinkedList<>();
-        for(Map.Entry<List<Character>, List<String>> e : map.entrySet()) {
-            List<String> list = new LinkedList<>(e.getValue());
-            res.add(list);
-        }
-        return res;
+        return new ArrayList<>(map.values());
     }
 
-    private List<Character> toCharSetSorted(String s) {
+    private String toCharSetSorted(String s) {
         char[] chars = s.toCharArray();
-        List<Character> characters = new ArrayList<>();
-        for (char aChar : chars) {
-            characters.add(aChar);
-        }
-        Collections.sort(characters);
-        return characters;
+        Arrays.sort(chars);
+        return new String(chars);
     }
 
     public static void main(String[] args) {
