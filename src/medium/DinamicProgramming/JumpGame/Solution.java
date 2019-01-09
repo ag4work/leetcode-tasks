5dac06d42c30996e232dbade96f774f524d84987
flamenco <ag4work@gmail.com>
2019-01-10 01:50:38 +0300

    jump game - still too slow

--- src/medium/DinamicProgramming/JumpGame/Solution.java
+++ src/medium/DinamicProgramming/JumpGame/Solution.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class Solution {
     public boolean canJump(int[] nums) {
-        if (nums.length == 1 && nums[0]==0)  return true;
+        if (nums.length == 1)  return true;
         List<Integer> collect = Arrays.stream(nums).boxed().collect(Collectors.toList());
         return check(collect);
     }
 
     boolean check(List<Integer> m) {
-        if (m.isEmpty()) return true;
-        if (m.size() == 1 && m.get(0)==0)  return true;
-        for (int i = 1; i <= m.get(0); i++) {
+        if (m.isEmpty() ) return false;
+        if (m.size() == 1) return true;
+        for (int i = m.get(0); i>0; i--) {
             List<Integer> newArr = shift(m, i);
             if (check(newArr)) {
                 return true;
@@ -33,8 +32,10 @@ class Solution {
         return res;
     }
     public static void main(String[] args) {
-//        System.out.println(new Solution().canJump(new int[]{2,3,1,1,4}));
-        System.out.println(new Solution().canJump(new int[]{2,0,0}));
+        System.out.println(new Solution().canJump(new int[]{2,3,1,1,4}));
+//        System.out.println(new Solution().canJump(new int[]{2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,
+//                2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,
+//                4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6}));
 //        System.out.println(new Solution().canJump(new int[]{3,2,1,0,4}));
     }
 }
