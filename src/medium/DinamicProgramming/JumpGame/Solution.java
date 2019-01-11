cb0f178ae4ff23b350cd91e807cb71f3a2cb9bf9
flamenco <ag4work@gmail.com>
2019-01-11 04:39:19 +0300

    jump game - still not enough fast

--- src/medium/DinamicProgramming/JumpGame/Solution.java
+++ src/medium/DinamicProgramming/JumpGame/Solution.java
@@ -1,38 +1,39 @@
 package medium.DinamicProgramming.JumpGame;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Solution {
+    List<Integer> m;
+    int len;
+    Boolean[] cache;
+
     public boolean canJump(int[] nums) {
         if (nums.length == 1)  return true;
-        List<Integer> collect = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        return check(collect);
+        m = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        len = m.size();
+        cache = new Boolean[len];
+        return check(0);
     }
 
-    boolean check(List<Integer> m) {
-        if (m.isEmpty() ) return false;
-        if (m.size() == 1) return true;
-        for (int i = m.get(0); i>0; i--) {
-            List<Integer> newArr = shift(m, i);
-            if (check(newArr)) {
+    boolean check(int k) {
+        if (k >= len) return false;
+        if (k  == len - 1) return true;
+        if (cache[k] != null) return false;
+        for (int i = m.get(k); i>0; i--) {
+            if (check(i+k)) {
                 return true;
             }
         }
         return false;
     }
 
-    private List<Integer> shift(List<Integer> m, int a) {
-        List<Integer> res = new ArrayList<>(m);
-        for (int i = 0; i < Math.min(m.size(), a); i++) {
-            res.remove(0);
-        }
-        return res;
-    }
+
     public static void main(String[] args) {
-        System.out.println(new Solution().canJump(new int[]{2,3,1,1,4}));
+        System.out.println(new Solution().canJump(new int[]{2,0,0,1,4}));
+
 //        System.out.println(new Solution().canJump(new int[]{2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,
 //                2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,
 //                4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6}));
