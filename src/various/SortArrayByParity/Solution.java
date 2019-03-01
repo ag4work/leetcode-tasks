ffbb87bf1eb6ef60807c1b48782ed8ca3a18bc3f
flamenco <ag4work@gmail.com>
2019-03-02 01:49:10 +0300

    Sort array by parity - with two pointers

--- src/various/SortArrayByParity/Solution.java
+++ src/various/SortArrayByParity/Solution.java
@@ -5,26 +5,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
-    public int[] sortArrayByParity(int[] A) {
-        List<Integer> list = new LinkedList<>();
-        for (int e : A) {
+    public int[] sortArrayByParity(int[] arr) {
+        int[] res = new int[arr.length];
+        int l = 0;
+        int r = arr.length - 1;
+        for (int e : arr) {
             if (e % 2 == 0) {
-                list.add(0, e);
+                res[l++] = e;
             } else {
-                list.add(e);
+                res[r--] = e;
             }
         }
-        return toIntArr(list);
+        return res;
     }
 
-    private int[] toIntArr(List<Integer> list) {
-        int[] arr = new int[list.size()];
-        int i = 0;
-        for (int e : list) {
-            arr[i++] = e;
-        }
-        return arr;
-    }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new Solution().sortArrayByParity(new int[]{3, 1, 2, 4})));
