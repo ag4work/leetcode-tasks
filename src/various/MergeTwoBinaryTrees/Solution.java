c73ff486267182656d33c0a22bcb33f2924f8530
flamenco <ag4work@gmail.com>
2019-03-03 01:04:08 +0300

    Merge Two Binary Trees

---
+++ src/various/MergeTwoBinaryTrees/Solution.java
@@ -0,0 +1,43 @@
+package various.MergeTwoBinaryTrees;
+
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        } else if (t1 == null) {
+            return t2;
+        } else if (t2 == null) {
+            return t1;
+        } else {
+            TreeNode t = new TreeNode(t1.val + t2.val);
+            t.left = mergeTrees(t1.left, t2.left);
+            t.right = mergeTrees(t1.right, t2.right);
+            return t;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        t1.left = new TreeNode(3);
+        t1.left.left = new TreeNode(5);
+        t1.right = new TreeNode(2);
+
+        TreeNode t2 = new TreeNode(2);
+        t2.left = new TreeNode(1);
+        t2.left.right = new TreeNode(4);
+        t2.right = new TreeNode(3);
+        t2.right.right = new TreeNode(7);
+        TreeNode treeNode = new Solution().mergeTrees(t1, t2);
+        int a = 1;
+
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
