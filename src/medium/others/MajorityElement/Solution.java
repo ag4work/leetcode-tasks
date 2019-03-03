834c100173ec44293be6a1c5d2d44fdc75d633b1
flamenco <ag4work@gmail.com>
2019-03-04 00:48:48 +0300

    Majority Element - on sorting

--- src/medium/others/MajorityElement/Solution.java
+++ src/medium/others/MajorityElement/Solution.java
@@ -1,20 +1,18 @@
 package medium.others.MajorityElement;
 
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int e : nums) {
-            m.put(e, m.getOrDefault(e,0) + 1);
-        }
-//        System.out.println(m);
-        Map.Entry<Integer, Integer> max = Collections.max(m.entrySet(), (a, b) -> a.getValue().compareTo(b.getValue()));
-        return max.getKey();
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
+
+
 
     public static void main(String[] args) {
         System.out.println(new Solution().majorityElement(new int[]{2,2,1,1,1,2,2}));
