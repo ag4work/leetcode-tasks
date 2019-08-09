d7a5bff5e294131528ecd02823a9b00793a9bf3a
agordeev <aleksei.gordeev@t-systems.com>
2019-08-09 22:44:18 +0300

    intersection 2 linked lists (smart solution)

--- src/easy_random/IntersectionTwoLinkedLists/Solution.java
+++ src/easy_random/IntersectionTwoLinkedLists/Solution.java
@@ -3,26 +3,51 @@ package easy_random.IntersectionTwoLinkedLists;
 
 public class Solution {
 
-
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) { return null; }
 
-        int la = len(headA);
-        int lb = len(headB);
+        ListNode pa = headA;
+        ListNode pb = headB;
 
-        ListNode longer = la > lb ? headA : headB;
-        ListNode smaller = la > lb ? headB : headA;
-        for (int i = 0; i < Math.abs(la - lb); i++) {
-            longer = longer.next;
-        }
-
-        while (longer != null) {
-            if (longer == smaller) return longer;
-            longer = longer.next;
-            smaller = smaller.next;
+        boolean pr1 = false;
+        boolean pr2 = false;
+        while(!(pr1 && pr2 && (pa == null))) {       //pr1 && pr2 &&
+            if (pr1 && pr2 && pa == pb) return pa;
+            if (pa != null) {
+                pa = pa.next;
+            } else if (!pr1) {
+                pr1 = true;
+                pa = headB;
+            }
+            if (pb != null) {
+                pb = pb.next;
+            } else if (!pr2){
+                pr2 = true;
+                pb = headA;
+            }
         }
         return null;
     }
+
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        if (headA == null || headB == null) { return null; }
+//
+//        int la = len(headA);
+//        int lb = len(headB);
+//
+//        ListNode longer = la > lb ? headA : headB;
+//        ListNode smaller = la > lb ? headB : headA;
+//        for (int i = 0; i < Math.abs(la - lb); i++) {
+//            longer = longer.next;
+//        }
+//
+//        while (longer != null) {
+//            if (longer == smaller) return longer;
+//            longer = longer.next;
+//            smaller = smaller.next;
+//        }
+//        return null;
+//    }
 
     private static int len(ListNode cur) {
         int l = 0;
@@ -48,25 +73,13 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-//        ListNode c = new ListNode(8);
-//        c.next = new ListNode(4);
-//        c.next.next = new ListNode(5);
-//
-//        ListNode a = new ListNode(4);
-//        a.next = new ListNode(1);
-//        a.next.next = c;
-//
-//        ListNode b = new ListNode(5);
-//        b.next = new ListNode(0);
-//        b.next.next = new ListNode(1);
-//        b.next.next.next = c;
-
         ListNode c = new ListNode(8);
         c.next = new ListNode(4);
         c.next.next = new ListNode(5);
 
-        ListNode a = null;
-
+        ListNode a = new ListNode(4);
+        a.next = new ListNode(1);
+        a.next.next = c;
 
         ListNode b = new ListNode(5);
         b.next = new ListNode(0);
@@ -75,8 +88,6 @@ public class Solution {
 
         print(a);
         print(b);
-        System.out.println(len(a));
-        System.out.println(len(b));
 
         System.out.println(new Solution().getIntersectionNode(a, b));
 //        print(a);
