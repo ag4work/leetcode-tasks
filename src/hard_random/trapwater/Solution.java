3b70e047d8b1a1938bbc7668c6c333d30d614491
flamenco <ag4work@gmail.com>
2019-11-04 03:11:25 +0300

    trapping water

--- src/hard_random/trapwater/Solution.java
+++ src/hard_random/trapwater/Solution.java
@@ -5,33 +5,58 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 class Solution {
+    static boolean allowLog = false;
     int[] m;
+
+    private void log(String s) {
+        if (allowLog) {
+            System.out.println(s);
+        }
+    }
+
     public int trap(int[] arr) {
+        if (arr.length < 3) return 0;
         int sum = 0;
         m = arr;
         int size = arr.length;
         Deque<Integer> s = new LinkedList<>();
         for (int i = 0; i < size; i++) {
-            System.out.println("i: " + i + " m[i] : " + m[i]);
+            log("i: " + i + " m[i] : " + m[i]);
             if (!s.isEmpty() && reachCurFirstLocalMaximum(s, m[i])) {
-                System.out.println("достигли пред максимума, стэк: "  + s);
+                log("достигли пред максимума, стэк: " + s);
                 long tmp = calcCurSum(s.pollLast(), i, m);
-                System.out.println("подсумма до пред максимума: " + tmp);
+                log("подсумма до пред максимума: " + tmp);
                 sum += tmp;
                 s.clear();
-                continue;
-            } else if (isLocalMax(i)) {
-                System.out.println("local max :" + m[i]);
-                updateStack(s, arr[i]);
-                System.out.println("проапдейтили стэк :" + s);
             }
+            if (isLocalMax(i)) {
+                log("local max :" + m[i]);
+                updateStack(s, i);
+                log("проапдейтили стэк :" + s);
+            }
+        }
+        if (s.size() > 1) {
+            sum += processStack(s, m);
         }
         return sum;
     }
 
+    private int processStack(Deque<Integer> s, int[] m) {
+        int res = 0;
+        LinkedList<Integer> list = (LinkedList<Integer>) s;
+        for (int i = 0; i < s.size() - 1; i++) {
+
+            res += calcCurSum(list.get(i), list.get(i + 1), m);
+
+        }
+        log("res = " + res);
+        return res;
+    }
+
+
     private long calcCurSum(Integer prevMaxInd, int curInd, int[] m) {
         long sum = 0;
-        int level = m[prevMaxInd];
+        int level = Math.min(m[prevMaxInd], m[curInd]);
         for (int i = prevMaxInd; i <= curInd; i++) {
             int tmp = level - Math.min(m[i], level);
             sum += tmp;
@@ -48,7 +73,8 @@ class Solution {
             } else {
                 break;
             }
-        } ;
+        }
+        ;
         s.addLast(curMaxI);
     }
 
@@ -67,7 +93,7 @@ class Solution {
 
     // also delete noot needed maximums whick greater than cur max
     boolean reachCurFirstLocalMaximum(Deque<Integer> s, int curMax) {
-        while(true) {
+        while (true) {
             Integer prevMax = s.peekLast();
             if (m[prevMax] <= curMax) {
                 if (s.size() == 1) {
@@ -81,28 +107,42 @@ class Solution {
         }
     }
 
+    static void srt(Object actual, Object expected) {
+        System.out.println(actual.equals(expected) ? "ok" : "expected " + expected + " but was " + actual);
+    }
+
     public static void main(String[] args) {
 
-        int[] mas = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] mas = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        srt(6, new Solution().trap(mas));
+
+         mas = new int[]{0};
+        srt(6, new Solution().trap(mas));
+//        Deque<Integer> d = new LinkedList<>();
+//        d.add(10);
+//        d.add(7);
+//        new Solution().processStack(d, mas);
+
 //        int[] mas = new int[]{2,4,3,2,1,0,5,2,6,0,2,2,3,1,1,0,1,2};
         System.out.println(Arrays.toString(mas));
         Solution s = new Solution();
         System.out.println(s.trap(mas));
 
 //        Deque<Integer> d = new LinkedList<>(Arrays.asList(8, 6, 4, 2));
-//        System.out.println(d);
-//        System.out.println(new Solution().reachCurFirstLocalMaximum(d, 1) + " " + d);
+//        log(d);
+//        log(new Solution().reachCurFirstLocalMaximum(d, 1) + " " + d);
 //        d = new LinkedList<>(Arrays.asList(8, 6, 4, 2));
-//        System.out.println(new Solution().reachCurFirstLocalMaximum(d, 2)+ " " + d);
+//        log(new Solution().reachCurFirstLocalMaximum(d, 2)+ " " + d);
 //        d = new LinkedList<>(Arrays.asList(8, 6, 4, 2));
-//        System.out.println(new Solution().reachCurFirstLocalMaximum(d, 3)+ " " + d);
+//        log(new Solution().reachCurFirstLocalMaximum(d, 3)+ " " + d);
 //        d = new LinkedList<>(Arrays.asList(8, 6, 4, 2));
-//        System.out.println(new Solution().reachCurFirstLocalMaximum(d, 6)+ " " + d);
+//        log(new Solution().reachCurFirstLocalMaximum(d, 6)+ " " + d);
 //        d = new LinkedList<>(Arrays.asList(8, 6, 4, 2));
-//        System.out.println(new Solution().reachCurFirstLocalMaximum(d, 7)+ " " + d);
+//        log(new Solution().reachCurFirstLocalMaximum(d, 7)+ " " + d);
 //        d = new LinkedList<>(Arrays.asList(8, 6, 4, 2));
-//        System.out.println(new Solution().reachCurFirstLocalMaximum(d, 8)+ " " + d);
+//        log(new Solution().reachCurFirstLocalMaximum(d, 8)+ " " + d);
 //        d = new LinkedList<>(Arrays.asList(8, 6, 4, 2));
-//        System.out.println(new Solution().reachCurFirstLocalMaximum(d, 9)+ " " + d);
+//        log(new Solution().reachCurFirstLocalMaximum(d, 9)+ " " + d);
     }
+
 }
