e8b33bb2b1c3b4f7d356dca2c1fa59c100e68d3f
flamenco <ag4work@gmail.com>
2019-11-05 13:44:13 +0300

    serialize tree - start

--- src/hard_random/submatrixSum/Solution.java
+++ src/hard_random/submatrixSum/Solution.java
@@ -8,19 +8,34 @@ class Solution {
     static boolean LOG = true;
 
     int[][] m;
+    int w;
+    int h;
+
 
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
         m = matrix;
+        h = m.length;
+        w = m[0].length;
+        return find(0,0, w, h, 0);
     }
 
     int find(int x1, int y1, int x2, int y2, int sum) {
+        int s = 0;
+        for (int y = y1; y <= y2; y++) {
+//            s = s +
+            for (int x = x1; x <= x2; x++) {
+                log("x : " + x + " y : " + y);
+            }
+        }
 
+
+        return 1;
     }
 
     public static void main(String[] args) {
         int[][] mas = new int[][] {{1,2}, {3,0}};
         Solution sol = new Solution();
-        int s = sol.find(0, 0, 0, 0, 1);
+        int s = sol.numSubmatrixSumTarget(mas, 1212);
         srt(s, 24);
     }
 
