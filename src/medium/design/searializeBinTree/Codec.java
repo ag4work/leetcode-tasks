1db9cd9ad236032ccf6507eb8e3ecf015d641e39
agordeev <aleksei.gordeev@t-systems.com>
2019-11-07 17:36:37 +0300

    serialize tree - finished

--- src/medium/design/searializeBinTree/Codec.java
+++ src/medium/design/searializeBinTree/Codec.java
@@ -5,9 +5,39 @@ import java.util.stream.Collectors;
 
 public class Codec {
 
+    private static final boolean NEEDLOG = false;
+
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        return "";
+        if (root == null) return "[]";
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int curNodeNumInLevel = 0;
+        int level = 0;
+        List<Integer> res = new ArrayList<>();
+        res.add(root.val);
+        while(!q.isEmpty()) {
+            Queue<TreeNode> tmpQ = new LinkedList<>();
+            while(!q.isEmpty()) {
+                TreeNode node = q.remove();
+
+                if (node != null) {
+                    tmpQ.add(node.left);
+                    tmpQ.add(node.right);
+                }
+            }
+            log("tmpQ : " + tmpQ);
+            log("count : " + tmpQ.stream().filter(r -> r != null).count());
+            if (tmpQ.stream().filter(r -> r != null).count() > 0) {
+                q = tmpQ;
+                for (TreeNode t : tmpQ) {
+                   res.add(t == null ? null : t.val);
+                }
+            }
+        }
+
+        return res.toString().replaceAll(" ", "");
     }
 
 
@@ -19,22 +49,22 @@ public class Codec {
         q.add(root);
         while(!q.isEmpty()) {
             Queue<TreeNode> tmpQ = new LinkedList<>();
-            System.out.println("root: " + root);
+            log("root: " + root);
 
-            System.out.println("q: " + q.stream().map(r -> r.val).collect(Collectors.toList()));
+            log("q: " + q.stream().map(r -> r.val).collect(Collectors.toList()));
             while(!q.isEmpty()) {
                 TreeNode node = q.remove();
                 i++;
                 if (i < m.size() && m.get(i) != null) {
                     node.left = new TreeNode(m.get(i));
                     tmpQ.add(node.left);
-                    System.out.println("i = " + i + " Added left: " + node.left.val);
+                    log("i = " + i + " Added left: " + node.left.val);
                 }
                 i++;
                 if (i < m.size() && m.get(i) != null) {
                     node.right = new TreeNode(m.get(i));
                     tmpQ.add(node.right);
-                    System.out.println("i = " + i + " Added right: " + node.right.val);
+                    log("i = " + i + " Added right: " + node.right.val);
 
                 }
             }
@@ -43,8 +73,15 @@ public class Codec {
         return root;
     }
 
+    private void log(String s) {
+        if (NEEDLOG) System.out.println(s);
+    }
+
     private List<Integer> toList(String... m) {
         List<Integer> res = new LinkedList<>();
+        if (m.length == 1 && m[0].equals("")) {
+            return Collections.emptyList();
+        }
         for (String s : m) {
             if (s.equals("null")) {
                 res.add(null);
@@ -56,6 +93,7 @@ public class Codec {
     }
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        if (data.equals("[]")) return null;
         String s = data.substring(1, data.length() - 1);
         String[] split = s.split(",");
         return deser0(toList(split));
@@ -65,12 +103,29 @@ public class Codec {
     public static void main(String[] args) {
         TreeNode root = createTree();
         Codec codec = new Codec();
-//        System.out.println(codec.serialize(root));
-//        codec.deserialize(codec.serialize(root));
         String data = "[5,4,7,3,null,2,null,-1,null,9,null]";
-        System.out.println(data);
-        TreeNode deserialize = codec.deserialize(data);
-        System.out.println(deserialize);
+        srt(codec.serialize(codec.deserialize(data)), data);
+//
+//
+        String data1 = "[1,2,3,null,null,4,5]";
+        srt(codec.serialize(codec.deserialize(data1)), data1);
+
+        String data2 = "[5,4,null,3,7,null,null,2,null]";
+        srt(codec.serialize(codec.deserialize(data2)), data2);
+
+        String data3 = "[]";
+        srt(codec.serialize(codec.deserialize(data3)), data3);
+
+
+
+//        String data = "[1,2,3,null,null,4,5]";
+//        System.out.println(data);
+//        TreeNode node = codec.deserialize(data);
+//        System.out.println(node);
+//        String serialized = codec.serialize(node);
+//
+//        System.out.println(serialized);
+
     }
 
     private static TreeNode createTree() {
@@ -81,6 +136,12 @@ public class Codec {
         t.right = new TreeNode(4);
         return t;
     }
+
+    static void srt(Object actual, Object expected) {
+        System.out.println(actual.equals(expected) ? "ok" : "expected " + expected + " but was " + actual);
+    }
+
+
 }
 
 class TreeNode {
